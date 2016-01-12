7a21615046c23161186ad8926f9aa698bfa03fb2
bray94 <braydenturner94@gmail.com>
2016-01-11 19:12:48 -0600

    final

--- public_html/survey.php
+++ public_html/survey.php
@@ -52,7 +52,7 @@ if (!mysql_query($sql)) {
 }
 
 mysql_close($link);
-// echo "<script>
-//              window.history.go(-1);
-//      </script>";
+echo "<script>
+             window.history.go(-1);
+     </script>";
 ?>
