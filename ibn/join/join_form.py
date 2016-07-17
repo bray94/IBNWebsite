f5b7ab4339276a494811b9e20ed7402a5b0026e9
bray94 <braydenturner94@gmail.com>
2016-07-17 17:38:41 -0500

    stuff

--- ibn/join/join_form.py
+++ ibn/join/join_form.py
@@ -2,7 +2,8 @@ from django import forms
 
 class JoinForm(forms.Form):
 	fullName = forms.CharField(max_length = 100)
-	email = forms.charField(max_leng = 100)
-	password = forms.CharField(widget = forms.PasswordInput())
+	email = forms.charField(max_length = 100)
+	password = forms.CharField(max_length = 100)
+	retypePassword = form.CharField(max_length = 100)
 
 
--- ibn/join/views.py
+++ ibn/join/views.py
@@ -6,8 +6,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def join(request):
+	if 
 	return render(request, 'join.html')
 
 
+def submit_join(request):
+	error = False
+    if 'fullName' in request.GET and 'email' in request.GET and 'password' in request.GET and 'retypePassword' in request.GET:
+        fullName = request.GET['fullName']
+        email = request.GET['email']
+        password = request.GET['password']
+        retypePassword = request.GET['retypePassword']
 
+        noName = False
+        noEmail = False
+        noPassword = False
+        noRetypePassword = False
+
+        if not fullName:
+            noName = True
+
+        if not email:
+            noEmail = True
+
+        if not password:
+            noPassword = True
+
+        if not retypePassword:
+            noRetypePassword = True
+
+        else:
+        	firstName = fullName.split()[0]
+            return render(request, 'under_construction.html',
+                {'name': firstName})
 
