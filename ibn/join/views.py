665501ee0595494e44a8fb4c6f36a9ed848a2cbc
bray94 <braydenturner94@gmail.com>
2016-07-17 18:20:06 -0500

    stuff

--- ibn/join/views.py
+++ ibn/join/views.py
@@ -4,15 +4,19 @@ from django.shortcuts import render
 
 #from join.join_form.py import JoinForm
 from django.http import HttpResponse
+import MySQLdb
 
 def join(request):
-	if request.method == 'GET':
+	if request.method == 'POST':
+		db = MySQLdb.connect("localhost","root","Nedyarb94","myibn" )
+		cursor = db.cursor()
+
 		error = False
-		if 'fullName' in request.GET and 'email' in request.GET and 'password' in request.GET and 'retypePassword' in request.GET:
-			fullName = request.GET['fullName']
-			email = request.GET['email']
-			password = request.GET['password']
-			retypePassword = request.GET['retypePassword']
+		if 'fullName' in request.POST and 'email' in request.POST and 'password' in request.POST and 'retypePassword' in request.POST:
+			fullName = request.POST['fullName']
+			email = request.POST['email']
+			password = request.POST['password']
+			retypePassword = request.POST['retypePassword']
 
 			noName = False
 			noEmail = False
@@ -24,8 +28,29 @@ def join(request):
 			if not password: noPassword = True
 			if not retypePassword: noRetypePassword = True
 
+			firstName , lastName = (fullName.split()[0],fullName.split()[1]) 
+
+			sql = """INSERT INTO users(firstName,
+         		lastName, email, password)
+         		VALUES ('%s', '%s', '%s', %s)""" % (firstName, lastName, email, password)
+
+         	failed = False
+			try:
+				# Execute the SQL command
+				cursor.execute(sql)
+				# Commit your changes in the database
+
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				db.rollback()
+				failed = True
+				
+			db.close()
+
 			else:
-				firstName = fullName.split()[0]
+				
+
 				return render(request, 'under_construction.html', {'name': firstName})
 	else:
 		return render(request, 'join.html')
