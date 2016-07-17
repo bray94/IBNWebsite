4d3858d4621178953653d8d55770cc3833136bf2
bray94 <braydenturner94@gmail.com>
2016-07-17 17:48:14 -0500

    stuff

--- ibn/join/views.py
+++ ibn/join/views.py
@@ -8,33 +8,25 @@ from django.http import HttpResponse
 def join(request):
 	if request.method == 'GET':
 		error = False
-	    if 'fullName' in request.GET and 'email' in request.GET and 'password' in request.GET and 'retypePassword' in request.GET:
-	        fullName = request.GET['fullName']
-	        email = request.GET['email']
-	        password = request.GET['password']
-	        retypePassword = request.GET['retypePassword']
+		if 'fullName' in request.GET and 'email' in request.GET and 'password' in request.GET and 'retypePassword' in request.GET:
+			fullName = request.GET['fullName']
+			email = request.GET['email']
+			password = request.GET['password']
+			retypePassword = request.GET['retypePassword']
 
-	        noName = False
-	        noEmail = False
-	        noPassword = False
-	        noRetypePassword = False
+			noName = False
+			noEmail = False
+			noPassword = False
+			noRetypePassword = False
 
-	        if not fullName:
-	            noName = True
+			if not fullName: noName = True
+			if not email: noEmail = True
+			if not password: noPassword = True
+			if not retypePassword: noRetypePassword = True
 
-	        if not email:
-	            noEmail = True
-
-	        if not password:
-	            noPassword = True
-
-	        if not retypePassword:
-	            noRetypePassword = True
-
-	        else:
-	        	firstName = fullName.split()[0]
-	            return render(request, 'under_construction.html',
-	                {'name': firstName})
+			else:
+				firstName = fullName.split()[0]
+				return render(request, 'under_construction.html', {'name': firstName})
 	else:
 		return render(request, 'join.html')
 
